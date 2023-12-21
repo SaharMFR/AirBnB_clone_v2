f39ffcf20ee7aecc11f039c62976240157d08331
SaharMFR <sweetysahora@gmail.com>
2023-12-20 16:26:31 -0800

    update in task-6

--- models/__init__.py
+++ models/__init__.py
@@ -7,7 +7,7 @@ import os
 storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
 
-if type_storage == "db":
+if storage_type == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
