3e849785caaafe8f7cf78ecc6a567fde9e117cd2
SaharMFR <sweetysahora@gmail.com>
2024-01-28 19:20:23 +0000

    Fixing pycodestyle in task-7 0x04

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,7 +14,8 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Returns rendered template `7-states_list.html`"""
-    return render_template('7-states_list.html', states=storage.all(State).values())
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
