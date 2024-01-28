1f7d5d51f5404ca5f19dcc40fbe779551b2d6cf0
SaharMFR <sweetysahora@gmail.com>
2024-01-28 16:22:57 +0000

    Fixing pycodestyle in task-0 0x04

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" Starts a Flask web application listening on `0.0.0.0`, port `5000` """
+"""
+Starts a Flask web application
+listening on `0.0.0.0`, port `5000`
+"""
 
 from flask import Flask
 
@@ -8,9 +11,9 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-	""" Returns `Hello HBNB!` """
-	return 'Hello HBNB!'
+    """ Returns `Hello HBNB!` """
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
